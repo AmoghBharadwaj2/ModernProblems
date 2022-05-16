4ede81e6d6fa4ff7d6cac87c1ae3c3df2946b261
Wenhao Deng <wenhaodeng2023@u.northwestern.edu>
2022-05-15 21:09:02 -0500

    Delete Prototype directory

--- Prototype/test.py
+++
@@ -1,6 +0,0 @@
-from transformers import GPT2Tokenizer, GPT2Model, pipeline
-token = GPT2Tokenizer.from_pretrained('gpt2')
-mod = GPT2Model.from_pretrained('gpt2')
-text = "Replace me by any text you'd like."
-pipe = pipeline(model=mod, tokenizer=token)
-pipe(text, num_return_sequences=5, max_length=30)
